1032b58bacdd5f4a64bf37aac44da1cbe6fe3a3b
gojicmiroslav <miroslavy2k@gmail.com>
2016-05-03 21:08:06 +0200

    Install Devise

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'database_cleaner'
+require 'devise'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
