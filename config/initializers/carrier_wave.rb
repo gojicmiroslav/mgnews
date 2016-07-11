b1033f1fe96f9dd7abfea48a9f9190036706c631
Miroslav Gojic <miroslavy2k@gmail.com>
2016-07-11 23:06:45 +0200

    Fix for Heroku - #6

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,12 +8,18 @@
 if Rails.env.production?
 	CarrierWave.configure do |config|
 		config.fog_credentials = {
-			provider: "AWS",
-			aws_access_key_id: ENV['S3_ACCESS_KEY'],
-			aws_secret_key_id: ENV['S3_SECRET_KEY'],
-			region:            ENV['S3_REGION']
+			:provider			=> "AWS",
+			:aws_access_key_id	=> ENV['S3_ACCESS_KEY'],
+			:aws_secret_key_id   => ENV['S3_SECRET_KEY'],
+			:region              => ENV['S3_REGION']
 		}
 
 		config.fog_directory = ENV['S3_BUCKET']
 	end
 end
+
+config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => Rails.configuration.s3_access_key_id,
+    :aws_secret_access_key  => Rails.configuration.s3_secret_access_key,
+  }
